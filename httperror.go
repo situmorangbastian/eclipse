ab7e14796ed1bd9eb3be3587999c9b9f675fb2c1
Bastian Situmorang <bastianpaskal@gmail.com>
2020-07-17 23:02:20 +0700

    feat: middleware for generate http error response with status code (#4)

---
+++ httperror.go
@@ -0,0 +1,34 @@
+package eclipse
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	log "github.com/sirupsen/logrus"
+)
+
+// ErrMiddleware returns custom middleware for Echo that generate HTTP error response
+// with HTTP status code.
+func ErrMiddleware() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			err := next(c)
+			if err == nil {
+				return nil
+			}
+
+			// Check error based on error type
+			switch err.(type) {
+			case ConstraintError:
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+			case NotFoundError:
+				return echo.NewHTTPError(http.StatusNotFound, err.Error())
+			case ConflictError:
+				return echo.NewHTTPError(http.StatusConflict, err.Error())
+			default:
+				log.Error(err)
+				return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
+			}
+		}
+	}
+}
