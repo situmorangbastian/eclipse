c2ff517bc0e8e11efd833a8828d7a2b4e6efdf9c
situmorangbastian <bastianpaskal@gmail.com>
2021-10-23 08:24:20 +0700

    feat: add logger

---
+++ logger.go
@@ -0,0 +1,69 @@
+package eclipse
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// WriterHook is a hook that writes logs of specified LogLevels to specified Writer
+// Based on: https://github.com/sirupsen/logrus/issues/678
+type WriterHook struct {
+	Writer    io.Writer
+	LogLevels []log.Level
+}
+
+// Fire will be called when some logging function is called with current hook
+// It will format log entry to string and write it to appropriate writer
+func (hook *WriterHook) Fire(entry *log.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+	_, err = hook.Writer.Write([]byte(line))
+	return err
+}
+
+// Levels define on which log levels this hook would trigger
+func (hook *WriterHook) Levels() []log.Level {
+	return hook.LogLevels
+}
+
+// SetupLogs initialize logger.
+func SetupLogs(levelStr string) {
+	log.SetFormatter(&log.JSONFormatter{
+		PrettyPrint: true,
+	})
+	log.SetReportCaller(true)
+
+	if levelStr == "" {
+		levelStr = "debug"
+	}
+
+	level, err := log.ParseLevel(levelStr)
+	if err != nil {
+		log.Fatal("levelStr is not well-set:", level)
+	}
+	log.SetLevel(level)
+
+	log.SetOutput(ioutil.Discard) // Send all logs to nowhere by default
+
+	log.AddHook(&WriterHook{ // Send logs with level higher than warning to stderr
+		Writer: os.Stderr,
+		LogLevels: []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+			log.ErrorLevel,
+			log.WarnLevel,
+		},
+	})
+	log.AddHook(&WriterHook{ // Send info and debug logs to stdout
+		Writer: os.Stdout,
+		LogLevels: []log.Level{
+			log.InfoLevel,
+			log.DebugLevel,
+		},
+	})
+}
