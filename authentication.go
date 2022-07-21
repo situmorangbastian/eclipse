5fff62cd0d1f9c59e5a1fe48c75ad4b6a23d12f1
Bastian Situmorang <bastianpaskal@gmail.com>
2022-07-21 20:49:42 +0700

    feat: add authentication (#21)

---
+++ authentication.go
@@ -0,0 +1,24 @@
+package eclipse
+
+import (
+	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+)
+
+func Authentication(user jwt.MapClaims) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			user, ok := c.Get("user").(*jwt.Token)
+			if !ok {
+				return echo.NewHTTPError(http.StatusUnauthorized, "token invalid/expired/required")
+			}
+			userClaims := user.Claims.(jwt.MapClaims)
+
+			c.SetRequest(c.Request().WithContext(NewCustomContext(c.Request().Context(), userClaims)))
+
+			return next(c)
+		}
+	}
+}
